ea120ed797e066bba81ef0bb1f6f71fd4ae86053
psykodan <dan.danielkelly@gmail.com>
2020-01-17 14:37:01 +0000

    funcBench update

--- aws/funcBench/observer.js
+++ aws/funcBench/observer.js
@@ -2,6 +2,8 @@ var MongoClient = require('mongodb').MongoClient;
 const request = require('request');
 var url = 'mongodb://mongodb5446kd:na3zez@danu7.it.nuigalway.ie:8717/mongodb5446';
 
+
+//128MB
 var start = Date.now();
 request('https://45vbcux00g.execute-api.eu-west-1.amazonaws.com/bench/funcbench128', { json: true }, (err, res, body) => {
   if (err) { return console.log(err); }
@@ -15,6 +17,99 @@ request('https://45vbcux00g.execute-api.eu-west-1.amazonaws.com/bench/funcbench1
   data.response = stop;
   data.runtime = runtime;
   data.memory = 128;
+  dbo.collection("AWS").insertOne(data, function(err, res) {
+    if (err) throw err;
+    console.log("1 document inserted");
+    db.close();
+  });
+}); 
+
+});
+
+
+//256MB
+var start = Date.now();
+request('https://45vbcux00g.execute-api.eu-west-1.amazonaws.com/bench/funcbench256', { json: true }, (err, res, body) => {
+  if (err) { return console.log(err); }
+  var stop = Date.now();
+  var runtime = stop - start
+  MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("mongodb5446");
+  var data = body;
+  data.request = start;
+  data.response = stop;
+  data.runtime = runtime;
+  data.memory = 256;
+  dbo.collection("AWS").insertOne(data, function(err, res) {
+    if (err) throw err;
+    console.log("1 document inserted");
+    db.close();
+  });
+}); 
+
+});
+
+//512MB
+var start = Date.now();
+request('https://45vbcux00g.execute-api.eu-west-1.amazonaws.com/bench/funcbench512', { json: true }, (err, res, body) => {
+  if (err) { return console.log(err); }
+  var stop = Date.now();
+  var runtime = stop - start
+  MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("mongodb5446");
+  var data = body;
+  data.request = start;
+  data.response = stop;
+  data.runtime = runtime;
+  data.memory = 512;
+  dbo.collection("AWS").insertOne(data, function(err, res) {
+    if (err) throw err;
+    console.log("1 document inserted");
+    db.close();
+  });
+}); 
+
+});
+
+//1024MB
+var start = Date.now();
+request('https://45vbcux00g.execute-api.eu-west-1.amazonaws.com/bench/funcbench1024', { json: true }, (err, res, body) => {
+  if (err) { return console.log(err); }
+  var stop = Date.now();
+  var runtime = stop - start
+  MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("mongodb5446");
+  var data = body;
+  data.request = start;
+  data.response = stop;
+  data.runtime = runtime;
+  data.memory = 1024;
+  dbo.collection("AWS").insertOne(data, function(err, res) {
+    if (err) throw err;
+    console.log("1 document inserted");
+    db.close();
+  });
+}); 
+
+});
+
+//2048MB
+var start = Date.now();
+request('https://45vbcux00g.execute-api.eu-west-1.amazonaws.com/bench/funcbench2048', { json: true }, (err, res, body) => {
+  if (err) { return console.log(err); }
+  var stop = Date.now();
+  var runtime = stop - start
+  MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("mongodb5446");
+  var data = body;
+  data.request = start;
+  data.response = stop;
+  data.runtime = runtime;
+  data.memory = 2048;
   dbo.collection("AWS").insertOne(data, function(err, res) {
     if (err) throw err;
     console.log("1 document inserted");
